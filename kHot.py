8ca672dd63dc77b9cdc1ed373b0b91585a8d58cb
bacarbland <nectarten@hotmail.com>
2021-09-25 11:52:41 -0500

    Update kHot.py
    Improved codding practices and rename

--- kHot.py
+++ kHot.py
@@ -1,2 +1,3 @@
-def KHot(df,cols,dropna=True):    
+def kHot(df,cols,dropna=False):    
+    from pandas import get_dummies
     return pd.get_dummies(df[cols],prefix=['']*len(cols),prefix_sep='',columns=cols, dummy_na=~dropna).groupby(level=0, axis=1).sum()
